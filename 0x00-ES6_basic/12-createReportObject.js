66ab7b5b3d03697a290031562527c18515fd7507
Leulseged <leulbekele191@gmail.com>
2022-12-24 11:55:38 +0300

    report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(empList) {
+      return Object.keys(empList).length;
+    },
+  };
+}
