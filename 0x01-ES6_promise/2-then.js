98db0121210fe3a45d9ce9e1b829e6faf2ea66ff
Leulseged <leulbekele191@gmail.com>
2022-12-24 19:38:16 +0300

    resolve promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
 export default function handleResponseFromAPI(promise) {
+  const res = { status: 200, body: 'success' };
   return promise
-    .then({ status: 200, body: 'success' })
+    .then(() => res)
     .catch(() => new Error())
     .finally(() => { console.log('Got a response from the API'); });
 }
