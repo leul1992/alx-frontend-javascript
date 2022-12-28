63efd615a02566e1314a126f9b19538f7b7e5073
Leulseged <leulbekele191@gmail.com>
2022-12-28 12:03:10 +0300

    implement class

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(newName) {
+    if (typeof (newName) !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(newLength) {
+    if (typeof (newLength) !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(newStudents) {
+    newStudents.forEach((element) => {
+      if (typeof (element) !== 'string') {
+        throw TypeError('Students must be an array of strings');
+      }
+    });
+    this._students = newStudents;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
