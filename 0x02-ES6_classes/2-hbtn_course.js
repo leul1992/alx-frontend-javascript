e2aa2bb500a137eb5b265e9ec43d82a7d20457a9
Leulseged <leulbekele191@gmail.com>
2022-12-28 16:22:06 +0300

    implement class

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,6 +1,6 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name(name);
+    this._name = name;
     this._length = length;
     this._students = students;
   }
