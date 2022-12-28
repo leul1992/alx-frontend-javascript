08b2a5fe7b74c61b2638b8f1c96b84a08ef258ff
Leulseged <leulbekele191@gmail.com>
2022-12-28 17:28:23 +0300

    implement pricing class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
