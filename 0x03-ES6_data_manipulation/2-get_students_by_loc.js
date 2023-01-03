78716889ea731fd5840c4577a1a727b1dae81b9d
Leulseged <leulbekele191@gmail.com>
2023-01-03 12:41:43 +0300

    filter array

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(lst, city) {
-  return lst.filter((obj) => obj.location === city);
+export default function getStudentsByLocation(getList, city) {
+  return getList.filter((cty) => cty.location === city);
 }
