b7b6ad1ba1b18a82993c7518d2dff16bf777452d
Leulseged <leulbekele191@gmail.com>
2023-01-05 18:34:44 +0300

    return string of a set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,22 @@
+export default function cleanSet(set, startString) {
+  let str = '';
+  let flag = false;
+  for (const stringSet of set) {
+    flag = false;
+    for (let i = 0; i < startString.length; i += 1) {
+      if (startString[i] !== stringSet[i]) {
+        break;
+      }
+      if (i === startString.length - 1) {
+        flag = true;
+      }
+    }
+    if (str !== '' && flag !== false) {
+      str = str.concat('-');
+    }
+    if (flag === true) {
+      str = str.concat(stringSet.substr(startString.length));
+    }
+  }
+  return str;
+}
