5ce9bb1b4ab37db52171b111f35fdc8c7caaa9b6
Leulseged <leulbekele191@gmail.com>
2023-01-06 09:49:33 +0300

    return array buffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-  
+  const buf = new ArrayBuffer(length);
+  const dataView = new DataView(buf);
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  dataView.setInt8(position, value);
+  return dataView;
 }
